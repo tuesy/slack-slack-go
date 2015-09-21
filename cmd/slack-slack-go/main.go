b28dfe0246cee66f9ad06b36ac14f20517202d5e
Jimmy Zhang <mankindforward@gmail.com>
2015-09-20 17:57:03 -0700

    back app from example

---
+++ cmd/slack-slack-go/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/tuesy/slack-slack-go/Godeps/_workspace/src/github.com/gin-gonic/gin"
+	"github.com/tuesy/slack-slack-go/Godeps/_workspace/src/github.com/russross/blackfriday"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.LoadHTMLGlob("templates/*.tmpl.html")
+	router.Static("/static", "static")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	})
+
+	router.GET("/mark", func(c *gin.Context) {
+		c.String(http.StatusOK, string(blackfriday.MarkdownBasic([]byte("**hi!**"))))
+	})
+
+	router.Run(":" + port)
+}
